bf3dc5844cfd21431239f080060e01c0fe59fbaa
Greg Supratman <agsupratman@gmail.com>
2023-01-21 12:24:13 -0600

    Add that function

--- hi.cpp
+++ hi.cpp
@@ -5,8 +5,11 @@ using std::endl;
 using std::cin;
 using std::string;
 
+void sayHi(string name) {
+    cout << "hi, " + name + "!" << endl;
+}
 int main() {
     string name;
     cin >> name;
-    cout << "hi, " + name + "!" << endl;
+    sayHi(name);
 }
